5d74e05405fb6b5231996c6dba4f00116e52c2ff
alejomjc <alejomjc2011@gmail.com>
2023-07-15 13:22:37 -0500

    Commit inicial

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, session
+
+app = Flask(__name__)
+app.secret_key = 'J1$7qMf0@9z2'
+
+@app.route('/')
+def main():
+    session['mascotas'] = session['mascotas'] if len(session['mascotas']) > 1 else []
+    print('session', session['mascotas'])
+    return render_template('index.html', mascotas=session['mascotas'])
+
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        nombre_mascota = request.form['nombreMascota']
+        nombre_dueno = request.form['nombreDueno']
+        edad_mascota = request.form['edadMascota']
+        tipo_mascota = request.form['tipoMascota']
+        adicional = request.form['adicional']
+
+        mascota = {'nombre': nombre_mascota,
+                   'dueno': nombre_dueno,
+                   'edad': edad_mascota,
+                   'tipo': tipo_mascota,
+                   'adicional': adicional}
+        session['mascotas'] = session['mascotas'] + [mascota]
+        return render_template('index.html', mascotas=session['mascotas'], mensaje='Registro exitoso')
+
+    return render_template('registro.html')
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Registro de mascotas
+
+Esta es una aplicación web de registro de mascotas construida con Flask.
+
+## Descripción
+
+Esta aplicación es un ejemplo básico de una aplicación web utilizando Flask, un framework de Python para el desarrollo de aplicaciones web.
+
+## Funcionalidades
+
+- Página de inicio con la presentación y listado de mascotas.
+- Pagina de Registro de mascotas.
+- Uso de plantillas HTML para una presentación dinámica.
+- Integración de estilos CSS básicos.
+
+## Requisitos
+
+- Python 3
+- Flask
+
+## Instalación
+
+1. Clona el repositorio en tu máquina local.
+2. Crear un entorno vitual con el siguiente comando: py -3 -m venv venv
+3. Activa tu entorno virtual con el siguiente comando: venv\Scripts\activate
+4. Instala las dependencias del proyecto ejecutando el siguiente comando: pip install -r requirements.txt
+
